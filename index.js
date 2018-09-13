6feabe0e5916358994f616f919209d18592c3fe0
oscar leon <first.liberal.thinker@gmail.com>
2018-09-13 18:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
-  while (array.length < facts.length) {
+  while (i < facts.length) {
     array.push(facts[i] + "!!!");
     i++;
   }
